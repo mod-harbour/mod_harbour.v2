874d04217969ca112d0e8dfee5e95a1ebe1e8e7a
Carles Aubia <carles9000@gmail.com>
2021-12-29 19:40:24 +0100

    go64 enchanced

--- go64.bat
+++ go64.bat
@@ -7,10 +7,15 @@ call "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Bu
 
 c:\harbour\bin\win\msvc64\hbmk2 mod_harbour.v2.hbp -comp=msvc64 
 
-if errorlevel 1 goto exit
+if errorlevel 1 goto error
 
-del output\win64\mod_harbour.v2.exp
-del output\win64\mod_harbour.v2.lib
+@del output\win64\mod_harbour.v2.exp
+@del output\win64\mod_harbour.v2.lib
+
+goto exit
+
+:error
+@echo *** Error compile ***
 
 :exit
 
