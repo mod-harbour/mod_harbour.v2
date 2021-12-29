39ec581da2223e9fc5891cf39df7663141d653a6
Carles Aubia <carles9000@gmail.com>
2021-12-29 19:34:56 +0100

    go64 enchanced

--- go64.bat
+++ go64.bat
@@ -1,10 +1,17 @@
 @echo off
 cls
 
-@del output\win64\*.* /Q
+@del output\win64\mod_harbour.v2.* /Q
 
 call "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x86_amd64
 
 c:\harbour\bin\win\msvc64\hbmk2 mod_harbour.v2.hbp -comp=msvc64 
+
+if errorlevel 1 goto exit
+
+del output\win64\mod_harbour.v2.exp
+del output\win64\mod_harbour.v2.lib
+
+:exit
 
 pause
