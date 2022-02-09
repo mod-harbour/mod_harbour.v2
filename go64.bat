078902b10db27be451d225ae40ea1ae03ae9e210
DIEGO H FAZIO <52408779+diegofazio@users.noreply.github.com>
2022-02-09 20:24:14 -0300

    Update v2.1

--- go64.bat
+++ go64.bat
@@ -2,15 +2,18 @@
 cls
 
 @del output\win64\mod_harbour.v2.* /Q
+@del output\win64\libmhapache.* /Q
 
 call "%ProgramFiles(x86)%\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x86_amd64
 
-c:\harbour\bin\win\msvc64\hbmk2 mod_harbour.v2.hbp -comp=msvc64 
+c:\harbour\bin\win\msvc64\hbmk2 mod_harbour.v2.1.hbp -comp=msvc64 
 
 if errorlevel 1 goto error
 
 @del output\win64\mod_harbour.v2.exp
 @del output\win64\mod_harbour.v2.lib
+@del output\win64\libmhapache.exp
+@del output\win64\libmhapache.lib
 
 goto exit
 
